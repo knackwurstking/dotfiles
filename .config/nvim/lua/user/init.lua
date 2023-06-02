3eef90cfaa403ee0e43177e11bf56cc5cd40f184
Udo Bauer <knackwurstking.tux@gmail.com>
2023-06-03 01:42:23 +0200

    disable formatting and colorscheme

--- .config/nvim/lua/user/init.lua
+++ .config/nvim/lua/user/init.lua
@@ -18,8 +18,8 @@ return {
   },
 
   -- Set colorscheme to use
-  colorscheme = "astrodark",
-  --colorscheme = "gruvbox",
+  --colorscheme = "astrodark",
+  colorscheme = "gruvbox",
 
   -- Diagnostics configuration (for vim.diagnostics.config({...})) when diagnostics are on
   diagnostics = {
@@ -32,7 +32,7 @@ return {
     formatting = {
       -- control auto formatting on save
       format_on_save = {
-        enabled = true, -- enable or disable format on save globally
+        enabled = false, -- enable or disable format on save globally
         allow_filetypes = { -- enable format on save for specified filetypes only
           -- "go",
         },
